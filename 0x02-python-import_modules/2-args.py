69237b1f82c7afb9efdca92a0f31440dedf4cfbb
gilbertoguta37@gmail.com <gilbertoguta37@gmail.com>
2023-06-08 16:11:24 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if numb == 1:
         print("{} arguments.".format(numb - 1))
     elif numb == 2:
-        prunt("{} argument:".format(numb - 1))
+        print("{} argument:".format(numb - 1))
     else:
         print("{} arguments:".format(numb - 1))
 
