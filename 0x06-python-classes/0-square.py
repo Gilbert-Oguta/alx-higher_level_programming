b07cc01e6a68ba87b78761b77949f78db5fc0612
gilbertoguta37@gmail.com <gilbertoguta37@gmail.com>
2023-06-27 16:43:23 -0700

    classses'

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defines an empty class Square"""
+
+
+class Square:
+    """empty class Square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines a class Square"""
+
+
+class Square:
+    """class Square that defines a square"""
+    __size = None
+
+    def __init__(self, size):
+        """Initialization with size"""
+        self.__size = size
