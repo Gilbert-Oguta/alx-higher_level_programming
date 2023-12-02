468db791c56597b24f766be8ad3033b000651e29
Gilbert Oguta <gilbertoguta37@gmail.com>
2023-12-02 09:59:04 +0300

    update

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-
-function add (a, b) {
-  return (a + b);
-}
-
 const { argv } = require('process');
-const num1 = parseInt(argv[2]);
-const num2 = parseInt(argv[3]);
-
-console.log(add(num1, num2));
+const a = Number(argv[2]);
+const b = Number(argv[3]);
+const add = (a, b) => a + b;
+console.log(add(a, b));
