2eb242cff3e669bb4e7b0c5cf7d49e86ded40a3d
Gilbert Oguta <gilbertoguta37@gmail.com>
2023-12-05 00:10:09 +0300

    update 2

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,7 +4,8 @@ const newDict = {};
 for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
   }
-  newDict[dict[key]].push(key);
 }
 console.log(newDict);
